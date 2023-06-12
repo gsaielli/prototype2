743cb10856d650da52596e1bd6e1acf39827186b
Giuseppe Simone Aielli <giuseppe@aielli.net>
2023-06-12 15:16:44 +0200

    run

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+export NODE_ENV=production
+
+cd /home/pi/timer2023
+node server.js &
+chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost
